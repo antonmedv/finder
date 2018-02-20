1deb9527d19216355b47502fd7b7bac17ae6f35a
Anton Medvedev <anton@medv.io>
2018-02-21 02:22:37 +0700

    Rename .com to .html

--- test/test.js
+++ test/test.js
@@ -2,14 +2,14 @@ import test from 'ava'
 import check from './helpers/check'
 
 test('github', t => {
-  check(t, __dirname + '/pages/github.com')
+  check(t, __dirname + '/pages/github.com.html')
 })
 
 test('stripe', t => {
-  check(t, __dirname + '/pages/stripe.com')
+  check(t, __dirname + '/pages/stripe.com.html')
 })
 
 test('deployer', t => {
-  check(t, __dirname + '/pages/deployer.org')
+  check(t, __dirname + '/pages/deployer.org.html')
 })
 
