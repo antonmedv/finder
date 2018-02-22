b18af7e7ba9651032cddf968c7a4e218f8708ed8
Anton Medvedev <anton@medv.io>
2018-02-22 12:59:51 +0700

    Add config tests

--- test/helpers/check.js
+++ test/helpers/check.js
@@ -1,15 +1,15 @@
 import selector from '../../dist'
 import {readFileSync} from 'fs'
 
-export default function check(t, file) {
-  document.write(readFileSync(file, 'utf8'))
+export default function check(t, html, config = void 0) {
+  document.write(html)
 
   const list = []
 
   // Bug in jsdom: svg can't be queried.
   for(let node of document.querySelectorAll('*:not(svg):not(path):not(circle):not(title):not(g):not(rect)')) {
 
-    const css = selector(node)
+    const css = selector(node, config)
 
     t.is(document.querySelectorAll(css).length, 1, `Selector "${css}" selects more then one node.`)
     t.is(document.querySelector(css), node, `Selector "${css}" selects another node.`)
--- test/test.js
+++ test/test.js
@@ -1,15 +1,40 @@
 import test from 'ava'
+import {readFileSync} from 'fs'
 import check from './helpers/check'
 
 test('github', t => {
-  check(t, __dirname + '/pages/github.com.html')
+  check(t, readFileSync(__dirname + '/pages/github.com.html', 'utf8'))
 })
 
 test('stripe', t => {
-  check(t, __dirname + '/pages/stripe.com.html')
+  check(t, readFileSync(__dirname + '/pages/stripe.com.html', 'utf8'))
 })
 
 test('deployer', t => {
-  check(t, __dirname + '/pages/deployer.org.html')
+  check(t, readFileSync(__dirname + '/pages/deployer.org.html', 'utf8'))
 })
 
+test('config:seed', t => {
+  const html = `
+  <div>
+    <span>
+      <p></p>
+    </span>
+  </div>
+  `
+  check(t, html)
+  check(t, html, {seedMinLength: 3})
+  check(t, html, {seedMinLength: 3, optimizedMinLength: 3})
+  check(t, html, {threshold: 2})
+})
+
+test('config:threshold', t => {
+  const html = `
+  <div>
+    <p></p>
+    <p></p>
+    <p></p>
+  </div>
+  `
+  check(t, html, {threshold: 1})
+})
